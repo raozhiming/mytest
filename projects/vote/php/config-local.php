de8f89aa99d9f6b8d6a8acf7c5e2f56ee35de130
raozhiming <raozhiming@elastos.org>
2018-07-04 18:11:01 +0800

    delete config

--- projects/vote/php/config-local.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('HOSTNAME','localhost:3306');
-define('USERNAME','root');
-define('DBPASSWORD','raozhiming2018');
-define('DATABASE','vote');
-
-?>
--- projects/vote/php/config-test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('HOSTNAME','localhost');
-define('USERNAME','root');
-define('DBPASSWORD','exfw3wse');
-define('DATABASE','vote');
-
-?>
